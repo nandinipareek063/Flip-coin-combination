6d7b9959ce22bbaf069f9d589ad86ef1ba63f40e
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-08 12:00:00 +0530

    Modified: Singlet combination is added

--- coinFlipCombination.sh
+++ coinFlipCombination.sh
@@ -1,12 +1,27 @@
-echo "Welcome to coin flip combination problem"
+#!/bin/bash
 
-
+declare -A  dict
 heads=0
 tails=0
-no=$((RANDOM%2))
-if [ $no -eq 1 ]
-then
-	echo "Heads win "
-else
-        echo "Tails win"
-fi
+read -p "Enter the  no of times you want to flip a coin " num
+for((i=1;i<=num;i++))
+do
+        no=$((RANDOM%2))
+        if [ $no -eq 1 ]
+        then
+        echo "H"
+        (( heads ++ ))
+        value=H
+        else
+        (( tails ++ ))
+        echo "T"
+        value=T
+        fi
+        dict[$i]="$value"
+done
+echo "Dictionary : ${dict[@]}"
+echo "heads count" $heads
+echo  "tails count" $tails
+echo "percentage of heads: " $((( heads * 100 ) / num ))
+echo "percentage of tails: " $((( tails * 100 ) / num ))
+
