3829769b9933bd542da0e4bc294872b4a7464c4e
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-08 12:15:29 +0530

    Modified: Doublet combination is added

--- coinFlipCombination.sh
+++ coinFlipCombination.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
-
-declare -A  dict
-heads=0
-tails=0
+declare -A  dictionary
+hh=0
+tt=0
+ht=0
+th=0
 read -p "Enter the  no of times you want to flip a coin " num
 for((i=1;i<=num;i++))
 do
-        no=$((RANDOM%2))
+        no=$((RANDOM%4))
         if [ $no -eq 1 ]
         then
-        echo "H"
-        (( heads ++ ))
-        value=H
+        echo "HH"
+        (( hh  ++ ))
+        value=HH
+        elif [ $no -eq 2 ]
+        then
+        echo "TT"
+        (( tt  ++ ))
+        value=TT
+        elif [ $no -eq 3 ]
+        then
+        echo "HT"
+        (( ht  ++ ))
+        value=HT
         else
-        (( tails ++ ))
-        echo "T"
-        value=T
+        (( th  ++ ))
+        echo "TH"
+        value=TH
         fi
         dict[$i]="$value"
 done
 echo "Dictionary : ${dict[@]}"
-echo "heads count" $heads
-echo  "tails count" $tails
-echo "percentage of heads: " $((( heads * 100 ) / num ))
-echo "percentage of tails: " $((( tails * 100 ) / num ))
+echo  "hh: " $hh
+echo  "tt: " $tt
+echo "ht :" $ht
+echo  "th: " $th
+echo "percentage of HH: " $((( hh * 100 ) / num ))
+echo "percentage of TT: " $((( tt * 100 ) / num ))
+echo "percentage of HT: " $((( ht * 100 ) / num ))
+echo "percentage of TH: " $((( th * 100 ) / num ))
+
 
